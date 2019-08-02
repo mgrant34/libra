0927005659daa283300cf535634981846e4a2c32
Mike Grant <mrg3443@gmail.com>
2019-08-01 21:40:54 -0400

    update network

--- client/src/network_launch.rs
+++ client/src/network_launch.rs
@@ -5,15 +5,17 @@ use client::{client_proxy::ClientProxy, commands::*};
 use logger::set_default_global_logger;
 use rustyline::{config::CompletionType, error::ReadlineError, Config, Editor};
 use std::option;
+use std::env;
 
 #[allow(clippy::new_without_default)]
 pub fn new() -> std::io::Result<()> {
 
-
+    let path = env::current_dir()?;
+    let static_files = format!("{}/client/src/trusted_peers.config.toml", path.display());
     let mut client_proxy = ClientProxy::new(
         "ac.testnet.libra.org",
         "8000",
-        "trusted_peers.config.toml",
+        &static_files,
         "",
         false,
         Some("".to_string()),
